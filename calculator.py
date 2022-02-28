b2b86595119c49f2e504d4be08b92b25a779a7cd
JusAduc <70166521+JusAduc@users.noreply.github.com>
2022-02-28 09:18:52 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -26,30 +26,37 @@ answer.grid(row=0, column=0, columnspan=5)
 
 def buttonClick(number):
     # answer.delete(0, END)
-    # answer.insert(0, "number")
-    i=0
-    i = i + 1
-    iStr = str(i)
-    print("Hello WOrld!" + iStr)
+    answer.insert("end", number)
+    print(number)
+
+# Make function to convert input to int and add it into each add, subtract, mul and div
+def numberStringToInt():
+    num1Str = answer.get()
+    num1 = int(num1Str)
+    print(num1)
+
+def buttonClickAdd():
+    numberStringToInt()
+    answer.delete(0, END)
 
 #region Creating the Buttons
-button1 = Button(root, text="1", width = 12, bg="#D3D3D3", command=buttonClick(1))
-button2 = Button(root, text="2", width = 12, bg="#D3D3D3", command=buttonClick(2))
-button3 = Button(root, text="3", width = 12, bg="#D3D3D3", command=buttonClick(3))
-button4 = Button(root, text="4", width = 12, bg="#D3D3D3", command=buttonClick(4))
-button5 = Button(root, text="5", width = 12, bg="#D3D3D3", command=buttonClick(5))
-button6 = Button(root, text="6", width = 12, bg="#D3D3D3", command=buttonClick(6))
-button7 = Button(root, text="7", width = 12, bg="#D3D3D3", command=buttonClick(7))
-button8 = Button(root, text="8", width = 12, bg="#D3D3D3", command=buttonClick(8))
-button9 = Button(root, text="9", width = 12, bg="#D3D3D3", command=buttonClick(9))
-button0 = Button(root, text="0", width=12, bg="#D3D3D3", command=buttonClick(0))
-buttonDot = Button(root, text=".", width=12, bg="#D3D3D3", command=buttonClick(0))
-buttonClear = Button(root, text="Clear", width=55, bg="#D3D3D3", command=buttonClick(0))
-buttonMul = Button(root, text="x", width=12, bg="#D3D3D3", command=buttonClick(0))
-buttonDiv = Button(root, text="%", width=12, bg="#D3D3D3", command=buttonClick(0))
-buttonAdd = Button(root, text="+", width=12, bg="#D3D3D3", command=buttonClick(0))
-buttonSub = Button(root, text="-", width=12, bg="#D3D3D3", command=buttonClick(0))
-buttonEqu = Button(root, text="=", width=12, bg="#D3D3D3", command=buttonClick(0))
+button1 = Button(root, text="1", width = 12, bg="#D3D3D3", command= lambda : buttonClick(1))
+button2 = Button(root, text="2", width = 12, bg="#D3D3D3", command= lambda : buttonClick(2))
+button3 = Button(root, text="3", width = 12, bg="#D3D3D3", command= lambda : buttonClick(3))
+button4 = Button(root, text="4", width = 12, bg="#D3D3D3", command= lambda : buttonClick(4))
+button5 = Button(root, text="5", width = 12, bg="#D3D3D3", command= lambda : buttonClick(5))
+button6 = Button(root, text="6", width = 12, bg="#D3D3D3", command= lambda : buttonClick(6))
+button7 = Button(root, text="7", width = 12, bg="#D3D3D3", command= lambda : buttonClick(7))
+button8 = Button(root, text="8", width = 12, bg="#D3D3D3", command= lambda : buttonClick(8))
+button9 = Button(root, text="9", width = 12, bg="#D3D3D3", command= lambda : buttonClick(9))
+button0 = Button(root, text="0", width=12, bg="#D3D3D3", command= lambda : buttonClick(0))
+buttonDot = Button(root, text=".", width=12, bg="#D3D3D3", command= lambda : buttonClick(0))
+buttonClear = Button(root, text="Clear", width=55, bg="#D3D3D3", command= lambda : buttonClick(0))
+buttonMul = Button(root, text="x", width=12, bg="#D3D3D3", command= lambda : buttonClick(0))
+buttonDiv = Button(root, text="%", width=12, bg="#D3D3D3", command= lambda : buttonClick(0))
+buttonAdd = Button(root, text="+", width=12, bg="#D3D3D3", command=buttonClickAdd)
+buttonSub = Button(root, text="-", width=12, bg="#D3D3D3", command= lambda : buttonClick(0))
+buttonEqu = Button(root, text="=", width=12, bg="#D3D3D3", command= lambda : buttonClick(0))
 
 #endregion
 
